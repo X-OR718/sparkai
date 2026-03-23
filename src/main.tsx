33baf4df9e3679885f5e5924a966ac0743ce4a83
X-OR718 <mohsabri10@gmail.com>
2026-03-22 21:18:40 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
-import { BlinkProvider } from '@blinkdotnew/react'
 import App from './App'
 import './index.css'
 
@@ -10,11 +9,9 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BlinkProvider projectId={import.meta.env.VITE_BLINK_PROJECT_ID}>
-      <QueryClientProvider client={queryClient}>
-        <Toaster position="top-right" />
-        <App />
-      </QueryClientProvider>
-    </BlinkProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster position="top-right" />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
